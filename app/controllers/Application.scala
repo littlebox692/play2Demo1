d7e965ffd0a87373f1b025a0ed376672e352f3f6
littlebox692 <hwgs420086157@163.com>
2017-09-03 23:08:29 +0800

    just test commit to remote branch

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -6,6 +6,7 @@ import play.api.mvc._
 object Application extends Controller {
 
   def index = Action {
+    println("just for test branch")
     Ok(views.html.index("Your new application is ready."))
   }
 
