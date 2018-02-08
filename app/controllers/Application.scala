e3e0418dbf7e8c0771222c180f427b76bceaf5fe
littlebox692 <hwgs420086157@163.com>
2018-02-08 22:14:25 +0800

    just test commit to remote branch

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -8,6 +8,8 @@ object Application extends Controller {
   def index = Action {
     println("test master merge!")
     println("merge branch into master")
+    val aa = "33333"
+    println(s"${aa}")
     Ok(views.html.index("Your new application is ready."))
   }
 
